ea2170f9b0f7e0e6aadcef170bd96266ea7b3ab2
jmt <jmt>
2015-09-22 13:53:22 -0500

    starting 1st project (due in 3 days! AHHH)

---
+++ src/ai/Hello.java
@@ -0,0 +1,10 @@
+package ai;
+
+class Hello
+{
+    public static void main(String args[])
+    {
+	System.out.println("Hello world!");
+    }
+}
+
---
+++ src/ai/Hi.java
@@ -0,0 +1,12 @@
+package ai;
+
+/**
+ * Created by jim on 9/22/15.
+ */
+public class Hi
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World! ...er I mean Hi there.");
+	}
+}
